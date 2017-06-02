8035a8a37a36df260c1b6bf82173531435751578
i9b4r1a9h5i7m6 <mohamedibrahim35@gmail.com>
2017-06-02 11:31:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-'use strict';
 
-// We’re setting up an extremely simple server here.
-var express =   require("express");  
-  
-var app =   express();  
+var app = express(); 
 
-app.get('/',function(req,res){  
-        res.sendFile(__dirname + "/index.html");  
-    });  
-    
-    
-  
-  
-app.listen(5000,function(){  
-    console.log("Server is running on port 2000");  
+
+// Tell express where to find static assets
+var path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+/* Routes */
+var routes = require('./routes');
+app.get('/', routes.home);
+app.get('/star_wars_episode/:episode_num?', routes.movie_single);
+app.get('*', routes.not_found);
+
+// Configure server to run on port 3000
+// app.listen('3000', function() {
+// 	console.log('Application running on localhost:3000');
+// });
+app.listen(process.env.PORT || 5000, function() {
+	console.log('Application running on localhost:3000');
 });
